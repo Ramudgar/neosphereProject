198be0e87ab090c80b480d1790807b29c59acee5
Ramudgar <ramudgary52@gmail.com>
2023-09-22 16:25:09 +0545

    api to update user

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -62,7 +62,9 @@ router.get("/users/getdata", (req, res) => {
 router.get("/user/getdata", async (req, res) => {
   try {
     const users = await User.find();
-    res.status(200).json({ msg: "data fetched successfully", success: true, users });
+    res
+      .status(200)
+      .json({ msg: "data fetched successfully", success: true, users });
   } catch (err) {
     res.status(500).json({ msg: err.message, success: false });
   }
@@ -70,7 +72,7 @@ router.get("/user/getdata", async (req, res) => {
 
 // api t Usero get user data by id
 router.get("/users/getdata/:id", (req, res) => {
-  User.findById(req.params.id)
+  User.findById({ _id: req.params.id })
 
     .then((data) => {
       res.json({ msg: "Data fetched", success: true, data });
@@ -78,6 +80,71 @@ router.get("/users/getdata/:id", (req, res) => {
     .catch((error) => {
       res.status(500).json({ msg: error, success: false });
     });
+});
+
+// api to update user data by user id also use async await
+router.put("/users/update/:id", async (req, res) => {
+  const data = req.body;
+  const user = await User.findById(req.params.id);
+  console.log(user);
+  if (!data) {
+    res.status(400).json({ msg: "Data not found" });
+    return;
+  }
+  if (!user) {
+    res.status(400).json({ msg: "User not found" });
+    return;
+  }
+
+  try {
+    user.name = data.name ? data.name : user.name;
+    user.email = data.email ? data.email : user.email;
+    user.password = data.password ? data.password : user.password;
+    user.role = data.role ? data.role : user.role;
+    user.contactNumber = data.contactNumber
+      ? data.contactNumber
+      : user.contactNumber;
+    const updatedUser = await user.save();
+    res.json({ msg: "Data updated", success: true, updatedUser });
+  } catch (error) {
+    res.status(500).json({ msg: error, success: false });
+  }
+});
+
+// the same api to update user data by user id also use async await but use findOneAndUpdate method
+router.put("/users/updatedata/:id", async (req, res) => {
+  const data = req.body;
+  const user_id = req.params.id;
+
+  if (!data) {
+    return res.status(400).json({ msg: "Data not found", success: false });
+  }
+
+  try {
+    const user = await User.findById(user_id);
+
+    if (!user) {
+      return res.status(404).json({ msg: "User not found", success: false });
+    }
+
+    const updatedFields = {
+      name: data.name || user.name,
+      email: data.email || user.email,
+      password: data.password || user.password,
+      role: data.role || user.role,
+      contactNumber: data.contactNumber || user.contactNumber,
+    };
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: user_id },
+      { $set: updatedFields },
+      { new: true }
+    );
+
+    res.json({ msg: "Data updated", success: true, updatedUser });
+  } catch (error) {
+    res.status(500).json({ msg: error.message, success: false });
+  }
 });
 
 module.exports = router;
