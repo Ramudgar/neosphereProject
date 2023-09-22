3d5fee75d97f2ada2ebaa90935b52057895f3a49
Ramudgar <ramudgary52@gmail.com>
2023-09-22 14:59:06 +0545

    get routes for user are completed

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -39,10 +39,45 @@ router.post("/user/savedataasync", async (req, res) => {
       age: data.age,
     });
     const savedUser = await user.save();
-    res.json({ msg: "Data inserted", success: true, data: savedUser });
+    res
+      .status(201)
+      .json({ msg: "Data inserted", success: true, data: savedUser });
   } catch (e) {
     res.status(500).json({ msg: e.message, success: false });
   }
+});
+
+// api to get all user data
+router.get("/users/getdata", (req, res) => {
+  User.find()
+    .then((data) => {
+      res.status(200).json({ msg: "Data fetched", success: true, data });
+    })
+    .catch((error) => {
+      res.status(500).json({ msg: error, success: false });
+    });
+});
+
+// api to get all users using async and await
+router.get("/user/getdata", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ msg: "data fetched successfully", success: true, users });
+  } catch (err) {
+    res.status(500).json({ msg: err.message, success: false });
+  }
+});
+
+// api t Usero get user data by id
+router.get("/users/getdata/:id", (req, res) => {
+  User.findById(req.params.id)
+
+    .then((data) => {
+      res.json({ msg: "Data fetched", success: true, data });
+    })
+    .catch((error) => {
+      res.status(500).json({ msg: error, success: false });
+    });
 });
 
 module.exports = router;
