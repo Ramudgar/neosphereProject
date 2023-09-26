9c6920d67fafd3b40b83f5b84a556c8f7cf7224a
Ramudgar <ramudgary52@gmail.com>
2023-09-26 15:01:17 +0545

    api for login is completd

--- Routes/RegisterLoginRoutes.js
+++ Routes/RegisterLoginRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
 const bcryptjs = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const jwt = require("sonwebtjoken");
 
 // router for register
 router.post("/users/register", (req, res) => {
@@ -38,5 +38,39 @@ router.post("/users/register", (req, res) => {
     res.status(500).json({ msg: e, success: false });
   }
 });
+
+
+router.post("/users/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      res
+        .status(400)
+        .json({ msg: "email or password is not correct", success: false });
+      return;
+    }
+    const isMatch = await bcryptjs.compare(password, user.password);
+    if (!isMatch) {
+      res
+        .status(400)
+        .json({ msg: "email or password is not correct", success: false });
+      return;
+    }
+
+    const token = jwt.sign({ _id: user._id }, "neosphere", {
+      expiresIn: "30d",
+    });
+
+    res.json({ msg: "login successful", success: true, token, data: user });
+  } catch (e) {
+    res.status(500).json({ msg: e, success: false });
+  }
+});
+
+
+
+
+
 
 module.exports = router;
